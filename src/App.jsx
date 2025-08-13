c45c9de8df2df22e898405f04854dcb6f02163fb
Mushaf Khalil <musawithyou@gmail.com>
2025-08-13 11:52:31 +0500

    Navbar.css & navbar.jsx: aligned logo and fixed hover effect

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,16 @@ import TailoredSection from './components/TailoredSection';
 import Customers from './components/Customers';
 import TestimonialsSection from './components/TestimonialSection';
 import WhatWeOffer from './components/whatWeoffer';
-import CustomPOSSection from './components/CustomPOSSection'; // <-- CORRECTED IMPORT
+import CustomPOSSection from './components/CustomPOSSection';
 import FAQSection from './components/FAQSection';
 import ContactFormSection from './components/ContactFormSection';
 import Footer from './components/Footer';
 
-// IMPORTANT: Choose one of these paths based on where you put PricingPage.jsx
-import PricingPage from './sections/PricingPage'; // OR: import PricingPage from './sections/PricingPage';
+import PricingPage from './sections/PricingPage'; 
 
 function App() {
   return (
-    <Router> {/* This must wrap everything */}
+    <Router> 
       <Navbar /> 
 
       <Routes>
@@ -38,7 +37,7 @@ function App() {
           }
         />
         <Route path="/pricing" element={<PricingPage />} />
-        {/* Add more routes here, e.g., <Route path="/kiosk" element={<KioskPage />} /> */}
+    
       </Routes>
 
       <Footer /> 
